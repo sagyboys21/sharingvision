7beff7738a1478780ea081b352fffb7e4478eef2
learningbydoing <sagyboys21@gmail.com>
2021-05-18 22:20:41 +0700

    validation

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,11 @@ module backend
 go 1.16
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
+	"github.com/go-playground/validator"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -11,10 +17,10 @@ var db *gorm.DB
 var err error
 
 type User struct {
-	Id       int    `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
+	Id       int    `validate:"omitempty,uuid"`
+	Username string `validate:"required,gte=3"`
+	Password string `validate:"required,gte=7"`
+	Name     string `validate:"required,gte=3"`
 }
 
 type Result struct {
@@ -32,5 +38,68 @@ func main() {
 		fmt.Println("Connection Established")
 	}
 
+	handleRequests()
+	// migrate table user
 	db.AutoMigrate(&User{})
+
+}
+func handleRequests() {
+	fmt.Println("Running at port 9999")
+	myRoute := mux.NewRouter().StrictSlash(true)
+
+	myRoute.HandleFunc("/getuser", getUser)
+	myRoute.HandleFunc("/adduser", addUser).Methods("POST")
+	myRoute.HandleFunc("/getuser", getUser).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":9999", myRoute))
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var user User
+	json.Unmarshal(payloads, &user)
+
+	// user := User{
+	// 	Username: "",
+	// 	Password: "",
+	// 	Name:     "agungcoba",
+	// }
+
+	validate := validator.New()
+	err := validate.Struct(user)
+
+	// message error
+	res1 := Result{Code: 212, Data: user, Message: err.Error()}
+	resulterr, err1 := json.Marshal(res1)
+	// message success
+	res2 := Result{Code: 200, Data: user, Message: "User Created"}
+	result, err := json.Marshal(res2)
+
+	if err1 == nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resulterr)
+	}
+
+	db.Create(&user)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	var users = []User{}
+
+	db.Find(&users)
+	res := Result{Code: 200, Data: users, Message: "Success Get Products"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
 }
